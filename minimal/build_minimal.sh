48543ae3930a59e7d5eb63e3386e46e335b4c53f
Michael Klimenko <mklimenko@golantelecom.co.il>
2018-09-28 14:04:48 +0300

    minimal cgr executables docker images

--- minimal/build_minimal.sh
+++ minimal/build_minimal.sh
@@ -2,9 +2,7 @@
 echo "Building CGRateS Minimal Dockers..."
 
 cd ./minimal
-# try to read binary versions (Ex. CGRateS 0.9.1~rc8 git+76dcc17 (2018-09-27T15:59:10+03:00))
-CGR_ENGINE_VER=$(bin/cgr-tester --version)
-echo "cgr-tester: "$CGR_ENGINE_VER
-# try to build a docker image
-docker build -t cgr-tester:${CGR_ENGINE_VER// /_} -f ./cgr-tester/Dockerfile .
-echo "Finished building cgr-tester"
+cp /home/golan/proj/bin/cgr-* ./bin
+# try to build a docker images
+BIN=cgr-tester make build login push
+echo "Finished building cgr binaries"
