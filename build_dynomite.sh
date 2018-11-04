3ae27abcab5eb54639438a0c6ace80cdb3c8eb1d
Michael Klimenko <mklimenko@golantelecom.co.il>
2018-11-04 17:39:16 +0200

    test decouple dynomite and redis

--- build_dynomite.sh
+++ build_dynomite.sh
@@ -10,7 +10,7 @@ make build
 
 #get git pull version from the latest dynomite 
 echo $(docker run -it --entrypoint dynomite gitlab-ptk.gt.local:5005/dynomite -V) > DYNOMITE_VERSION
-echo $(cat DYNOMITE_VERSION | sed -r 's/^This\sis\sdynomite-OOO-\d\d-(.+)$/\1/') > DYNOMITE_VERSION_TAG
+echo $(cat DYNOMITE_VERSION | sed -r 's/^.+dynomite-(.+)$/\1/') > DYNOMITE_VERSION_TAG
 
 #Tag with actual version
 make tag
