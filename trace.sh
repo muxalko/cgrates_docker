89a048b94e3ce7387299bcea6414730efbde7424
michael_klimenko_ <mklimenko@golantelecom.co.il>
2020-01-20 02:15:53 +0200

    fix tracer timezone

--- trace.sh
+++ trace.sh
@@ -5,7 +5,7 @@ then
 	if [[ "$2" == "start" ]]
 	then
 		echo "Trying to start ngrep $1 container"
-		docker run -it --net=container:$1 --rm --name=ngrep_$1 gitlab-ptk.gt.local:5005/tracer:1.1 ngrep -t -W byline $3 -d any
+		docker run -it --net=container:$1 --rm -e TZ=Asia/Jerusalem --name=ngrep_$1 gitlab-ptk.gt.local:5005/tracer:1.1 ngrep -t -W byline $3 -d any
 		echo "ngrep $1 container exited"
 		
 	elif [[ "$2" == "stop" ]]
